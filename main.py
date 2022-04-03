dd6378b6e26baf7dd93de998af26628cb1aa6836
nacho <ignacio.contrerascabello@gmail.com>
2022-04-03 17:48:46 +0200

    añadido asiento y tarjeta

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
+import sqlite3
+import pandas as pd
+
+con = sqlite3.connect('data.db')
+cur = con.cursor()
+
 class User:
-    def _init__(self, name):
+    def __init__(self, name):
         self.name = name
 
     def buy_seat(self, seat):
         pass
 
-    def use_card(self, card):
-        pass
+    def use_card(self, card, prize):
+        if card.validate():
+            current_balance = cur.execute("select balance from cards where number = self.card.number")
+
+
 
 class Seat:
 
-    def __init__(self, seat_id, price):
-        self.seat_id = seat_id
-        self.price = price
+    def __init__(self, seat):
+        self.seat = seat
 
     def is_free(self):
-        pass
+        seat_f = cur.execute(f"select taken from cinema where seat_id = '{self.seat}'").fetchone()[0]
+        print(seat_f)
+        if seat_f == '1':
+            return False
+        else:
+            return True
 
     def occupy(self):
-        pass
+        if self.is_free():
+            cur.execute(f"update cinema set taken = 1 where seat_id = '{self.seat}'")
+            cur.execute('commit')
+            print(f'seat {self.seat} changed from free to occupied')
+        else:
+            print('Seat is currently occupied')
+
 
 class Card:
 
@@ -28,8 +47,19 @@ class Card:
         self.cvc = cvc
         self.holder = holder
 
+    def __repr__(self):
+        return f'Card tipo {self.tipo}, con numero {self.number} y cvc {self.cvc} ' \
+               f'cuyo titular se llama {self.holder}'
+
     def validate(self):
-        pass
+        valid = cur.execute(f"""select * from cards where type = '{self.tipo}' and number = {self.number} 
+                            and cvc = {self.cvc} and holder = '{self.holder}'""").fetchall()
+
+        if len(valid) > 0:
+            return True
+        else:
+            return False
+
 
 class Ticket:
     def __init__(self, id, user, price, seat):
@@ -40,3 +70,10 @@ class Ticket:
 
     def to_pdf(self, path):
         pass
+
+user = User(name='Francisco Donaire')
+seat = Seat(seat='B3')
+seat2 = Seat(seat='B2')
+card = Card(tipo='Master Card', number=23456789, cvc=234, holder='Marry Smith')
+seat.occupy()
+seat2.occupy()
